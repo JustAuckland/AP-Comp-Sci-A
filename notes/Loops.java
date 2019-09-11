72cda8ee040969767ca2abbe7c5a42bdbe1f9e9f
JustAuckland <31050482+JustAuckland@users.noreply.github.com>
2019-09-11 10:49:02 -0700

    9/11/19 (2)

--- notes/Loops.java
+++ notes/Loops.java
@@ -7,6 +7,21 @@
  */
 public class Loops{
     public static void main1(){
+        for (int row = 1; row < 6; row++){
+            for(int col = 1; col <= row; col++){
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main2(){
         
+    }
+    public static void whileLoops(){
+    int counter = 10;
+    while (counter > 0){
+        System.out.println("The count is " + counter );
+        counter--;
+    }
     }
 }
