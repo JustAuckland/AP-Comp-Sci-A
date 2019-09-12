a0c1b3961aab12fb84168d0b744740df8b825a14
JustAuckland <31050482+JustAuckland@users.noreply.github.com>
2019-09-12 10:50:25 -0700

    9/12/19

---
+++ Auckland V - Lucky 7's/Lucky7s.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Lucky7s here.
+ *
+ * @author Auckland V
+ * @version 9/12/19
+ */
+import java.lang.Math;
+public class Lucky7s{
+    public static void Sevens(){
+        int stopRound = 0;
+        int rounds = 0;
+        int money = 15;
+        int mostMoney = 0;
+        double num = Math.random()*6 + 1;
+        if (num != 0){
+            
+        }
+    }
+}
