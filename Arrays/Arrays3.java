608755080063e5436f7cdbdfb2e684943eb7b1e0
JustAuckland <31050482+JustAuckland@users.noreply.github.com>
2019-10-29 10:50:33 -0700

    10/29/19

---
+++ Arrays/Arrays3.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Arrays3 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Arrays3{
+    public static void main(){
+        //Challenge 1
+        //Create an array of doubles (20 numbers)
+        double[] doubleArray = {3456.5,24654.87,35454.3,35456.9,52354,1854,154,654,654,41654.56,324,654,354,654,54.679,654,645,354,654.567,654,};
+        
+        //Challenge 2
+        //Find the sum of all the elements in the array
+        for(int i = 0; i < doubleArray.length; i++){
+            
+        }
+    }
+}
---
+++ notes/Array.java
@@ -0,0 +1,27 @@
+
+/**
+ * Write a description of class Array here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Array{
+    public static void main(){
+        int[] newArray;
+        newArray = new int[10];
+        int[] array2 = {1,2,3,4,5,6,7,8,9,10};
+        array2[4] = 9001;
+        System.out.println(array2);
+        newArray[0] = 3;
+        newArray[1] = 4;
+        newArray[2] = 9;
+        newArray[3] = 5;
+        newArray[4] = 0;
+        String[] string = {"hello", "there", "world"};
+    }
+    public static void printArray(ont[] myArray){
+        for (int i = 0; i < mrArray.length; 1++){
+            System.out.println(myArray[i]);
+        }
+    }
+}
